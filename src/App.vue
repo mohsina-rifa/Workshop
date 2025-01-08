6443f904906048963423603aac778b9006f3fce0
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-01-08 17:12:10 +0600

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Member from './components/Member.vue';
+</script>
+
+<template>
+  <Member/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/types/auth.ts
@@ -0,0 +1,8 @@
+export type Employee = {
+    firstname: string;
+    lastname: string;
+    fathername: string;
+    country: string;
+    contact: string;
+    password: string;
+}
