0f1c027d9b6fbeec106f6e41a7f860a67f920587
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-01-08 19:50:21 +0600

    Added reusable dropdown

--- src/components/AddForm.vue
+++ src/components/AddForm.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
 import {ref} from "vue";
 import TextInput from "../reusable/TextInput.vue";
+import Dropdown from "../reusable/Dropdown.vue";
 
 type Student = {
-  firstName: string,
-  lastName: string,
+  firstName: string;
+  lastName: string;
+  country: string
 }
 
 const student = ref<Student>({
   firstName: "",
   lastName: "",
+  country: ""
 })
+
+const countryList = ref(['Bangladesh', 'Germany'])
 </script>
 
 <template>
@@ -19,9 +24,11 @@ const student = ref<Student>({
     <div class="col-6">
       <p>First name: {{student.firstName}}</p>
       <p>Last Name: {{student.lastName}}</p>
+      <p>Country: {{student.country}}</p>
       <form>
         <TextInput id="firstName" v-model="student.firstName" label="First Name:" />
         <TextInput id="lastName" v-model="student.lastName" label="Last Name:" />
+        <Dropdown id="country" v-model="student.country" label="Select Country:" :options="countryList"/>
       </form>
     </div>
   </div>
---
+++ src/reusable/Dropdown.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+defineProps({
+    label: {
+        type: String,
+        required: true,
+    },
+    id: {
+        type: String,
+        required: true,
+    },
+    options: {
+        type: Array,
+        required: true,
+    },
+    modelValue: {
+        type: String,
+        required: true,
+    },
+})
+
+const emit = defineEmits(["update:modelValue"])
+
+const emitHandler = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    emit("update:modelValue", target.value);
+}
+</script>
+
+<template>
+    <div>
+        <label :for="id" class="form-label">{{ label }}</label>
+        <div class="mb-4 input-group">            
+            <select class="form-select" :id="id" :value="modelValue" @input="emitHandler">
+                <option v-for="(option, index) in options" :value="option" :key="index"> {{option}} </option>
+                <!-- <option value="bangladesh" selected>Bangladesh</option>
+                <option value="germany">Germany</option>
+                <option value="use">USA</option> -->
+                
+            </select>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
