de235e5e0e9025d51f08b0edc25318254a43ac51
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-01-13 16:13:59 +0600

    Main Page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
-<script setup lang="ts">
-// import Member from './components/Member.vue';
-import AddForm from "./components/AddForm.vue";
-// import Employee from './components/Employee.vue';
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <!--  <Member/>-->
-  <AddForm />
-  <!-- <Employee /> -->
+  <div class="container-lg d-flex min-vh-100">
+    <div class="row justify-content-center align-items-center">
+      <div class="col-md-7 text-center text-md-start">
+        <h1>
+          <div class="display-2">Workshop</div>
+          <div class="display-5 text-muted">Have Your Work Recognized.</div>
+        </h1>
+        <p class="lead my-4 text-muted">
+          Join millions of others in the community!
+        </p>
+        <a href="#" class="btn btn-secondary btn-lg">Sign up</a>
+      </div>
+      <div class="col-md-5 text-center d-none d-md-block">
+        <img
+          class="img-fluid"
+          src="../src/assets/workshop.svg"
+          alt="workshop"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
