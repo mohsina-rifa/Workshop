5f8672ea8567adf1402d5a8172a5605b87073077
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-01-13 12:35:12 +0600

    Button Validation: attempt-3

--- src/reusable/Button.vue
+++ src/reusable/Button.vue
@@ -1,10 +1,48 @@
 <script setup lang="ts">
+import ButtonValidate from '../validation/ButtonValidate.vue';
+import ContactValidate from "../validation/ContactValidate.vue";
+import CountryValidate from "../validation/CountryValidate.vue";
+import NameValidate from "../validation/NameValidate.vue";
+import PasswordValidate from "../validation/PasswordValidate.vue";
+
 const props = defineProps({
   label: {
     type: String,
     required: true,
   },
+  isContactGiven: {
+    required: true,
+    type: Boolean,
+  },
+  isCountryGiven: {
+    required: true,
+    type: Boolean,
+  },
+  isNameGiven: {
+    required: true,
+    type: Boolean,
+  },
+  isPasswordGiven: {
+    required: true,
+    type: Boolean,
+  },
+  isEligible: {
+    type: Boolean,
+    required: true,
+  }
 });
+
+const emit = defineEmits(["update:isEligible"]);
+
+const emitHandler = () => {
+  if (props.isEligible) {
+    emit("update:isEligible", true);
+    return true;
+  } else {
+    emit("update:isEligible", false);
+    return false;
+  }
+};
 </script>
 
 <template>
@@ -14,9 +52,23 @@ const props = defineProps({
       data-mdb-ripple-init
       class="btn btn-dark btn-lg btn-block"
       type="button"
+      v-if="emitHandler()"
     >
       {{ props.label }}
     </button>
+
+    <button
+      data-mdb-button-init
+      data-mdb-ripple-init
+      class="btn btn-dark btn-lg btn-block"
+      type="button"
+      disabled
+      v-else
+    >
+      {{ props.label }}
+    </button>
+
+    <ButtonValidate :isContactGiven="isContactGiven" :isCountryGiven="isCountryGiven" :isNameGiven="isNameGiven" :isPasswordGiven="isPasswordGiven" />
   </div>
 </template>
 
--- src/validation/ButtonValidate.vue
+++ src/validation/ButtonValidate.vue
@@ -1,5 +1,49 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
 
-<template></template>
+const props = defineProps({
+  isContactGiven: {
+    required: true,
+    type: Boolean,
+  },
+  isCountryGiven: {
+    required: true,
+    type: Boolean,
+  },
+  isNameGiven: {
+    required: true,
+    type: Boolean,
+  },
+  isPasswordGiven: {
+    required: true,
+    type: Boolean,
+  },
+});
+
+const allFieldsGiven = computed(() => {
+  return (
+    props.isContactGiven &&
+    props.isCountryGiven &&
+    props.isNameGiven &&
+    props.isPasswordGiven
+  );
+});
+
+const emit = defineEmits(["isEligible"]);
+
+if (allFieldsGiven) {
+  emit("isEligible", true);
+} else {
+  emit("isEligible", false);
+}
+</script>
+
+<template>
+  <div>
+    <p class="text-warning" v-if="allFieldsGiven===false">
+      Please fill up all the input fields.
+    </p>
+  </div>
+</template>
 
 <style scoped></style>
