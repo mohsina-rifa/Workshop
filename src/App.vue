95b0f780ee1595f5ac1f6524f74e93a0784eaeb3
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-01-21 15:56:50 +0600

    Kanban Board: Template

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import KanbanBoard from './components/KanbanBoard.vue';
 import Navbar from './components/Navbar.vue';
+
 
 </script>
 
 <template>
   <Navbar />
   <router-view /> 
+  <KanbanBoard />
 </template>
 
 <style scoped></style>
---
+++ src/components/KanbanBoard.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+//logic here
+</script>
+
+<template>
+  <div class="container-fluid py-4">
+    <h1 class="text-center mb-4">My To-Do List</h1>
+    <div class="kanban-board justify-content-center">
+      <div class="kanban-column" id="todo">
+        <h4>Assigned</h4>
+        <div class="tasks" data-status="todo"></div>
+        <div class="add-task-form">
+          <input
+            type="text"
+            class="form-control mb-2"
+            placeholder="New task title"
+          />
+          <select class="form-select mb-2">
+            <option value="low">Low Priority</option>
+            <option value="medium">Medium Priority</option>
+            <option value="high">High Priority</option>
+          </select>
+          <button class="btn btn-primary btn-sm w-100">Add Task</button>
+        </div>
+      </div>
+      <div class="kanban-column" id="in-progress">
+        <h4>In Progress</h4>
+        <div class="tasks" data-status="in-progress"></div>
+      </div>
+      <div class="kanban-column" id="done">
+        <h4>Completed</h4>
+        <div class="tasks" data-status="done"></div>
+      </div>
+      <div class="kanban-column" id="done">
+        <h4>Unsuccessful</h4>
+        <div class="tasks" data-status="done"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
