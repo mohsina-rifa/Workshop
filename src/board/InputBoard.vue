0aaa3a380a98a1df8b7ecc41b1812f3c9dce05a1
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-01-22 19:44:56 +0600

    Board functionalities distribution between components

---
+++ src/board/InputBoard.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+//input via modal here
+</script>
+
+<template></template>
+
+<style scoped></style>
---
+++ src/board/OutputBoard.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+//show board from here
+</script>
+
+<template></template>
+
+<style scoped></style>
