1408534c129e58781b59275b87ed72083a79d6d1
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-01-24 12:05:20 +0600

    New component to handle Modal-logic

--- src/board/InputBoard.vue
+++ src/board/InputBoard.vue
@@ -1,5 +1,51 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+  isVisible: {
+    type: Boolean,
+    required: true,
+  },
+});
 
-<template></template>
+const emit = defineEmits(["close"]);
+</script>
+
+<template>
+  <div v-if="props.isVisible" class="modal fade show" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Add New Task</h5>
+          <button
+            type="button"
+            class="btn-close"
+            @click="emit('close')"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter task title"
+          />
+          <textarea
+            class="form-control mb-2"
+            placeholder="Enter task description"
+            rows="3"
+          />
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            @click="emit('close')"
+          >
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Save Task</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style scoped></style>
--- src/components/ToDoBoard.vue
+++ src/components/ToDoBoard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-//import InputBoard from "../board/InputBoard.vue";
+import InputBoard from "../board/InputBoard.vue";
 import OutputBoard from "../board/OutputBoard.vue";
 
 const isModalVisible = ref(false);
@@ -26,40 +26,8 @@ const closeModal = () => {
     <div>
       <OutputBoard />
     </div>
-    <div
-      v-if="isModalVisible"
-      class="modal fade show"
-      tabindex="-1"
-      style="display: block; background-color: rgba(0, 0, 0, 0.5)"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Add New Task</h5>
-            <button
-              type="button"
-              class="btn-close"
-              @click="closeModal"
-            ></button>
-          </div>
-          <div class="modal-body">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Enter task title"
-            />
-            <textarea
-              class="form-control mb-2"
-              placeholder="Enter task description"
-              rows="3"
-            />
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary">Close</button>
-            <button type="button" class="btn btn-primary">Save Task</button>
-          </div>
-        </div>
-      </div>
+    <div>
+      <InputBoard :isVisible="isModalVisible" @close="closeModal" />
     </div>
   </div>
 </template>
@@ -76,5 +44,10 @@ const closeModal = () => {
 
 .d-flex button {
   margin-right: 300px;
+}
+
+.modal {
+  display: block;
+  background-color: rgba(0, 0, 0, 0.5);
 }
 </style>
