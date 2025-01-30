3d9a10688c3c8d07f8ff907122c86fb29e1075c4
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-01-30 18:53:01 +0600

    Template for status-modal

--- src/board/StatusInputBoard.vue
+++ src/board/StatusInputBoard.vue
@@ -1,5 +1,33 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <div v-if="props.isVisible" class="modal fade show" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Add New Category</h5>
+          <button type="button" class="btn-close" @click="emit('close')"></button>
+        </div>
+        <div class="modal-body">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter task title"
+            v-model="newTask.taskTitle"
+            required
+          />
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" @click="emit('close')">
+            Close
+          </button>
+          <button type="button" class="btn btn-primary" @click="createTask">
+            Save Task
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style scoped></style>
