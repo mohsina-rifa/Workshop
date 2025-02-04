17c58ef25ff9f8cfc0ae4e7aea2bf69ae717436a
anish0m <mohsina.tabassum.rifa@gmail.com>
2025-02-04 23:11:37 +0600

    Fetch task-list & status-list by store

--- src/reusable/Doughnut.vue
+++ src/reusable/Doughnut.vue
@@ -1,4 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useTaskStore } from "../stores/taskStore";
+
+const props = defineProps({
+  userID: {
+    type: String,
+    required: true,
+  },
+});
+
+const taskStoreInstance = useTaskStore();
+
+onMounted(async () => {
+  await taskStoreInstance.fetchTasks(props.userID);
+  await taskStoreInstance.fetchStatuses();
+});
+
+const allTasks = computed(() => taskStoreInstance.getTaskList);
+const allStatus = computed(() => taskStoreInstance.getStatusList);
+
+const tasksBasedOnStatus = () => {
+  //
+}
+</script>
 
 <template></template>
 
