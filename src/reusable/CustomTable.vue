4637452c7c2800f8fddadf2332afb8e4810bcefb
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-02-07 15:43:21 +0600

    Declared props for Table

--- src/reusable/CustomTable.vue
+++ src/reusable/CustomTable.vue
@@ -1,4 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { PropType } from 'vue';
+
+import type { Column } from '../types/auth'
+
+const props = defineProps({
+  columns: {
+    type: Array as PropType<Column[]>,
+    required: true
+  },
+  dataset: {
+    type: Array,
+    required: true
+  },
+  isInverted: {
+    type: Boolean,
+    required: false
+  }
+})
+</script>
 
 <template>
   <div class="mt-4">
